19aecaffe4ec1f63ab96675e635cd15c3b268ed4
Andrii Peliak <vseprav@gmail.com>
2024-06-02 01:13:31 +0300

    add home page

---
+++ src/config/AppPaths.ts
@@ -0,0 +1,6 @@
+const AppPaths = {
+    home: '/',
+    about: '/about',
+};
+
+export default AppPaths;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import PageTemplate from "../templates/PageTemplate";
+
+const Home = () => {
+  return (
+      <PageTemplate content={<h1>Home Page</h1>} sidebar={<h1>КНИГА ПАМ’ЯТІ ЛЕМКІВЩИНИ 1944-1946</h1>} />
+  );
+}
+
+export default Home;
